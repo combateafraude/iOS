1fd12f8ab4543dee3cdb6ae2c3be63c3f1b26cee
Frederico Hansel dos Santos Gassen <fhgassen@inf.ufsm.br>
2020-06-05 16:21:23 -0300

    Create the releases branch

---
+++ document-detector/DocumentDetector.framework/Headers/DocumentDetector.h
@@ -0,0 +1,19 @@
+//
+//  DocumentDetector.h
+//  DocumentDetector
+//
+//  Created by Tiago Portaluppi on 26/09/19.
+//  Copyright © 2019 Combate a Fraude. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DocumentDetector.
+FOUNDATION_EXPORT double DocumentDetectorVersionNumber;
+
+//! Project version string for DocumentDetector.
+FOUNDATION_EXPORT const unsigned char DocumentDetectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocumentDetector/PublicHeader.h>
+
+
