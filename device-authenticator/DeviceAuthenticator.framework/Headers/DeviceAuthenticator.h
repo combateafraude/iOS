deacc81703b55f685ac07d4c5ab898764a15b806
Fernando Fiorentin <fernando@mobsolution.com.br>
2020-07-14 10:24:54 -0300

    Add DeviceAuthenticator v0.1.0

---
+++ device-authenticator/DeviceAuthenticator.framework/Headers/DeviceAuthenticator.h
@@ -0,0 +1,19 @@
+//
+//  CFSecurity.h
+//  CFSecurity
+//
+//  Created by Mob Solution on 12/02/20.
+//  Copyright © 2020 Combate a Fraude. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CFSecurity.
+FOUNDATION_EXPORT double DeviceAuthenticatorVersionNumber;
+
+//! Project version string for CFSecurity.
+FOUNDATION_EXPORT const unsigned char DeviceAuthenticatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CFSecurity/PublicHeader.h>
+
+
