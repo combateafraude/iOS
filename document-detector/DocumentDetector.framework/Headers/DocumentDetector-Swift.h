ac6b3d9714f783ba198ac55ae1f43fc0fded6e60
Daniel Seitenfus <daniel.seitenfus@hotmail.com>
2021-10-19 17:03:25 -0300

    Add DocumentDetector 4.8.0

--- document-detector/DocumentDetector.framework/Headers/DocumentDetector-Swift.h
+++ document-detector/DocumentDetector.framework/Headers/DocumentDetector-Swift.h
@@ -189,6 +189,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
 @import AVFoundation;
+@import CoreGraphics;
 @import CoreMedia;
 @import ObjectiveC;
 @import UIKit;
@@ -354,6 +355,33 @@ SWIFT_CLASS("_TtC16DocumentDetector20DocumentDetectorStep")
 @interface DocumentDetectorStep : NSObject
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
+@end
+
+@class NSNumber;
+
+SWIFT_CLASS("_TtC16DocumentDetector30DocumentDetectorViewController")
+@interface DocumentDetectorViewController : UIViewController <AVCapturePhotoCaptureDelegate, AVCaptureVideoDataOutputSampleBufferDelegate>
+- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
+- (void)viewDidLoad;
+/// configure the size of camera layer
+- (void)viewDidLayoutSubviews;
+- (void)viewWillAppear:(BOOL)animated;
+/// stop session when controller is closed
+- (void)viewWillDisappear:(BOOL)animated;
+- (void)viewDidAppear:(BOOL)animated;
+/// receive camera data
+- (void)captureOutput:(AVCaptureOutput * _Nonnull)output didOutputSampleBuffer:(CMSampleBufferRef _Nonnull)sampleBuffer fromConnection:(AVCaptureConnection * _Nonnull)connection;
+- (void)captureOutput:(AVCapturePhotoOutput * _Nonnull)output willCapturePhotoForResolvedSettings:(AVCaptureResolvedPhotoSettings * _Nonnull)resolvedSettings;
+- (void)captureOutput:(AVCapturePhotoOutput * _Nonnull)output didFinishProcessingPhoto:(AVCapturePhoto * _Nonnull)photo error:(NSError * _Nullable)error;
+@end
+
+
+SWIFT_CLASS("_TtC16DocumentDetector19DocumentOverlayView")
+@interface DocumentOverlayView : UIView
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
+- (nonnull instancetype)initWithFrame:(CGRect)frame SWIFT_UNAVAILABLE;
 @end
 
 
