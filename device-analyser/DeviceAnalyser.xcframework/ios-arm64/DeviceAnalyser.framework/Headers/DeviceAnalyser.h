8141b7d23a67f83c74d05cc4aa434f2c47562ce9
Daniel Seitenfus <daniel.seitenfus@hotmail.com>
2022-05-06 17:10:03 -0300

    Add DeviceAnalyser 1.0.0

---
+++ device-analyser/DeviceAnalyser.xcframework/ios-arm64/DeviceAnalyser.framework/Headers/DeviceAnalyser.h
@@ -0,0 +1,19 @@
+//
+//  DeviceAnalyser.h
+//  DeviceAnalyser
+//
+//  Created by CAF on 06/05/22.
+//  Copyright © 2022 Combate à Fraude. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeviceAnalyser.
+FOUNDATION_EXPORT double DeviceAnalyserVersionNumber;
+
+//! Project version string for DeviceAnalyser.
+FOUNDATION_EXPORT const unsigned char DeviceAnalyserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceAnalyser/PublicHeader.h>
+
+
