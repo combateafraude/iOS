c55e1fee28fae38aec8410f96b64aedcbaf6412a
Miguel Cruz <79615087+MiguelXCruz@users.noreply.github.com>
2023-03-17 12:49:31 -0600

    Add Identity 1.0.0

---
+++ identity/Identity.xcframework/ios-arm64_x86_64-simulator/Identity.framework/Headers/Identity.h
@@ -0,0 +1,18 @@
+//
+//  Identity.h
+//  Identity
+//
+//  Created by Felipe Marques Ramos on 06/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Identity.
+FOUNDATION_EXPORT double IdentityVersionNumber;
+
+//! Project version string for Identity.
+FOUNDATION_EXPORT const unsigned char IdentityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Identity/PublicHeader.h>
+
+
