6f6f41229f3ab427394d42f9d648973eea32e32d
gabrielCaldeiraMartins <gabriel.martins@caf.io>
2023-08-22 14:21:27 -0300

    Add FaceLivenessSPM 1.0.1

---
+++ FaceAuthSPM/Sources/FaceAuthSPM/FaceAuthSPM.swift
@@ -0,0 +1,6 @@
+public struct FaceAuthSPM {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ FaceAuthSPM/Tests/FaceAuthSPMTests/FaceAuthSPMTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FaceAuthSPM
+
+final class FaceAuthSPMTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FaceAuthSPM().text, "Hello, World!")
+    }
+}
