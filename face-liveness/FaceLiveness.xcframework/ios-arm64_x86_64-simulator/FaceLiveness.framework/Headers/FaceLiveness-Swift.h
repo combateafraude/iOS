fb45e4cd358c5e37d59216077325e9be3d311bf5
gabrielCaldeiraMartins <gabriel.martins@caf.io>
2023-09-06 14:03:47 -0300

    Add FaceLiveness 2.7.0

--- face-liveness/FaceLiveness.xcframework/ios-arm64_x86_64-simulator/FaceLiveness.framework/Headers/FaceLiveness-Swift.h
+++ face-liveness/FaceLiveness.xcframework/ios-arm64_x86_64-simulator/FaceLiveness.framework/Headers/FaceLiveness-Swift.h
@@ -300,6 +300,7 @@ SWIFT_CLASS("_TtCC12FaceLiveness15FaceLivenessSDK5Build")
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
 
+
 #endif
 #if defined(__cplusplus)
 #endif
@@ -609,6 +610,7 @@ SWIFT_CLASS("_TtCC12FaceLiveness15FaceLivenessSDK5Build")
 @interface Build : NSObject
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
+
 
 #endif
 #if defined(__cplusplus)
