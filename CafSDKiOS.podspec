18b9657e4ca6e3241d773c06e7bc00127fc0130d
Henrique Luís Oliveira Marques <henrique.marques@caf.io>
2025-07-22 12:08:35 -0300

    Add CafSDKiOS 2.0.0-beta7

--- CafSDKiOS.podspec
+++ CafSDKiOS.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'CafSDKiOS'
-  s.version          = '2.0.0-beta6'
+  s.version          = '2.0.0-beta7'
   s.summary          = 'Caf iOS SDK'
   s.homepage         = 'https://github.com/combateafraude/caf-ios-sdk'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
--- Frameworks/DocumentDetector.xcframework/ios-arm64/DocumentDetector.framework/Headers/DocumentDetector-Swift.h
+++ Frameworks/DocumentDetector.xcframework/ios-arm64/DocumentDetector.framework/Headers/DocumentDetector-Swift.h
@@ -432,14 +432,14 @@ SWIFT_CLASS("_TtC16DocumentDetector22DocumentDetectorResult")
 @end
 
 
-SWIFT_CLASS("_TtC16DocumentDetector19DocumentDetectorSdk")
+SWIFT_CLASS("_TtC16DocumentDetector19DocumentDetectorSdk") SWIFT_DEPRECATED_MSG("This method of using the SDK is deprecated. Use CafSDK as the single entry point for all functionalities. Access the integration documentation at: https://docs.caf.io/caf-sdk/ios/getting-started-with-the-sdk")
 @interface DocumentDetectorSdk : NSObject
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
 @end
 
 
-SWIFT_CLASS("_TtCC16DocumentDetector19DocumentDetectorSdk10CafBuilder")
+SWIFT_CLASS("_TtCC16DocumentDetector19DocumentDetectorSdk10CafBuilder") SWIFT_DEPRECATED_MSG("This method of using the SDK is deprecated. Use CafSDK as the single entry point for all functionalities. Access the integration documentation at: https://docs.caf.io/caf-sdk/ios/getting-started-with-the-sdk")
 @interface CafBuilder : NSObject
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
